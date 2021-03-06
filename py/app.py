f9d06c26cfe0050407e0ac6371e74caab72a8665
April <alagnevall@gmail.com>
2021-03-06 15:23:14 -0600

    adding index.html for API documentation to the py template folder.  Adding start of flask app.py for API

---
+++ py/app.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+# engine = create_engine("")
+
+# reflect an existing database into a new model
+# Base = automap_base()
+# # reflect the tables
+# Base.prepare(engine, reflect=True)
+
+# # Save reference to the table
+# Adoption = Base.classes.adoption
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return render_template("index.html", mars=scrape)
+    
+
+@app.route("/Adoption/1.0.2/animals")
+def names():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query all animals
+    results = session.query().all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    all_names = list(np.ravel(results))
+
+    return jsonify(all_names)
