878d7cd089643f873c744888a8cb92310df102bd
April <alagnevall@gmail.com>
2021-03-09 21:46:33 -0600

    Flask code working

--- py/app.py
+++ py/app.py
@@ -1,18 +1,18 @@
 import numpy as np
-import json
+import pandas as pd
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from config import user, password 
 from flask import Flask, jsonify, render_template, request, Response
-# from flask_marshmallow import Marshmallow
+
 
 #################################################
 # Database Setup
 #################################################
 engine = create_engine(f"postgresql://{user}:{password}@localhost:5432/pets_db")
-# print(engine)
+print(engine)
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
@@ -22,7 +22,8 @@ Base.prepare(engine, reflect=True)
 print(Base.classes.keys())
 
 # Adoption = Base.classes.all_pets
-Adoption = Base.classes.dogs
+adoption = Base.classes.adoptable_pets
+# zips = Base.classes.postcodes
 
 #################################################
 # Flask Setup
@@ -46,14 +47,14 @@ def animals():
     session = Session(engine)
 
     # Query all animals
-    results = session.query(Adoption)
+    results = session.query(adoption)
+
+    data = pd.read_sql(results.statement, engine)
+    petdata = data.to_dict(orient="records")
+
+    return jsonify(petdata)
+
     session.close()
-
-    # Convert list of tuples into normal list
-    # all_names = list(np.ravel(results))
-
-    # return(f'{type(results)}')
-    return Response(json.dumps(results), mimetype='application/json')
-
+  
 if __name__ == "__main__":
     app.run(debug=True)
