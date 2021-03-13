7cbe4c3abdb402198b1e8e182a373a84e2e8f72d
alagnevall <alagnevall@gmail.com>
2021-03-12 22:39:11 -0600

    working carousel with filters

--- js/Aprillogic.js
+++ js/Aprillogic.js
@@ -14,19 +14,36 @@ var myMap = L.map("map", {
     id: "mapbox/streets-v11",
     accessToken: api_key
   }).addTo(myMap);
-  
+
+let selection = "%" 
+let animalSelector = d3.select("div.dropdown-menu")
+  .selectAll("a")
+  .on("click", function(event){
+    console.log(d3.select(this).attr("value"))
+     selection =d3.select(this).attr("value");
+     updatePage();
+  })
+
+
+
   // Store API query variables
-  var baseURL = "http://localhost:5000/Adoption/1.0.2/animals?";
+  let baseURL = "http://localhost:5000/Adoption/1.0.2/animals?";
     
   // Assemble API query URL
-  var url = baseURL;
   
   // Grab the data with d3
-  d3.json(url, function(response) {
+  function updatePage(){
+    let url = baseURL + "type=" + selection;
+    console.log(url)
+    d3.json(url, function(response) {
   
     // Create a new marker cluster group
-    var markers = L.markerClusterGroup();
-  
+    let markers = L.markerClusterGroup();
+    let photoData = []
+    myMap.removeLayer(markers);
+    var clearPhotoData = d3.select("div.carousel-inner");
+    clearPhotoData.selectAll("div").remove()
+
     // Loop through data
     for (var i = 0; i < response.length; i++) {
   
@@ -34,23 +51,45 @@ var myMap = L.map("map", {
       var location = response[i].address;
       var city = response[i].city;
       var state = response[i].state;
-  
+      var photos = response[i].photo;
+       
+      if (photos !="none"){
+        photoData.push(photos)
+      }  
+
       // Check for location property
       if (location) {
-        d3.json(`http://www.mapquestapi.com/geocoding/v1/address?key=${mq_key}&location=${location} ${city} ${state}`, function(address){
-            markers.addLayer(L.marker([address.results[0].locations[0].latLng.lat, address.results[0].locations[0].latLng.lng]));
-            //   .bindPopup(response[i].address));
-
-        })
+        // d3.json(`http://www.mapquestapi.com/geocoding/v1/address?key=${mq_key}&location=${location} ${city} ${state}`, function(address){
+        //   markers.addLayer(L.marker([address.results[0].locations[0].latLng.lat, address.results[0].locations[0].latLng.lng]));
+        // })
+          // .bindPopup(response[i].address);
+          
         // Add a new marker to the cluster group and bind a pop-up
         // markers.addLayer(MQ.geocode({map: myMap}).search(location));
-
+        
       }
-  
+      
     }
-  
+    // console.log(photoData)
+    var slides = d3.select("div.carousel-inner")
+    .selectAll("img")
+    .data(photoData);
+    
+    slides.enter()
+    .append("div")
+    .attr("class", "carousel-item")
+    .append("img")
+    .attr("class", "d-block mx-auto")
+    .attr("src", function(d){return d;})
+    .attr("width", 200);
+    // .attr("height", 50)
+
+    d3.select("div.carousel-item")
+      .attr("class", "carousel-item active")
+    
     // Add our marker cluster layer to the map
     myMap.addLayer(markers);
   
   });
- 
+}
+updatePage();
